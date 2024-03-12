2e6d1bd4b9fa0aca1f82bc7fd4cd890cb9728fe5
guivicentep <guivicente.gvp@hotmail.com>
2024-03-12 11:28:22 -0300

    wip: Criação do componente header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
+    <html lang="pt" className={inter.variable}>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
+
+// 08:59
